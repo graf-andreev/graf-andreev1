2e9741f054b28b1cb23025d8585bbff0274035c4
Maksim Andreev <46867573+graf-andreev@users.noreply.github.com>
2019-07-09 14:11:51 +0700

    Update script.js

--- test/script.js
+++ test/script.js
@@ -5,4 +5,11 @@ $(".phone_mask").mask("+7(999)999-99-99");
     $(".email").inputmask("email")
   });
 
- 
+ var today = new Date();
+var today = new Date();
+var tomorrow = new Date(today.getTime() + (24 * 60 * 60 * 1000));
+var dayTomorrow = tomorrow.getDate(); 
+var monthTomorrow = tomorrow.getMonth() + 1;
+var yearTomorrow = tomorrow.getFullYear(); 
+
+    document.getElementById('data').value='2019-07-15';
