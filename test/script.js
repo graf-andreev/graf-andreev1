cf8e8b86090b38c65a5e9097cd210a739a475b9f
Maxim <stat1c@ngs.ru>
2019-07-09 22:31:38 +0700

    Update script.js

--- test/script.js
+++ test/script.js
@@ -48,6 +48,6 @@ $(document).ready(function() {
       };
 
       formData = JSON.stringify(formData);
-      alert(JSON.stringify(formData));
+      console.log(JSON.stringify(formData));
   });
 });
