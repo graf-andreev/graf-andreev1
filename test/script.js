2238c7f6684fea296e86d6bdfa82c40fc947e1df
Maxim <stat1c@ngs.ru>
2019-07-09 22:23:04 +0700

    Update script.js

--- test/script.js
+++ test/script.js
@@ -1,8 +1,12 @@
+//jquery mask form input
+
 $(".phone_mask").mask("+7(999)999-99-99");
 
 $(document).ready(function() {   
   $(".email").inputmask("email");
 });
+
+//next day for data
 
 var today = new Date();
 var tomorrow = new Date(today.getTime() + (24 * 60 * 60 * 1000));
@@ -20,13 +24,16 @@ if (monthTomorrow <= 9) {
 
 dataAdd.value=`${yearTomorrow}-${monthTomorrow}-${dayTomorrow}`;
 
+//inn input and json data
+
 $(document).ready(function() {
   $("#form").submit(function(e) {
       e.preventDefault();
             
       var innValue = String($('#inn').val());
-      if (innValue.length !== 10 || innValue.length !== 12) {
-        alert("Inn value is invalid");
+      if (innValue.length !== 10 && innValue.length !== 12) {
+        document.getElementById('inn').style.borderColor ='red';
+        alert('ИНН должен содержать 10 или 12 цифр')
         var innInput = document.getElementById('inn');
         innInput.validity = false;
         return;
@@ -41,9 +48,6 @@ $(document).ready(function() {
       };
 
       formData = JSON.stringify(formData);
-      console.log(JSON.stringify(formData));
+      document.write(JSON.stringify(formData));
   });
 });
-
-
-
