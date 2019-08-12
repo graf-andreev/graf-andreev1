5de2fcfeab0e26cf17334fc47bed94f1c542dedd
Maksim Andreev <46867573+graf-andreev@users.noreply.github.com>
2019-08-12 14:59:05 +0700

    .

--- english_school/script.js
+++ english_school/script.js
@@ -12,32 +12,19 @@ $('.owl-carousel').owlCarousel({
             items:2
         },
         1000:{
-            items:3
+            items:4
         }
     }
 })
-
-var owl=$(".owl-carousel");
-	owl.owlCarousel();
-	//$(".next") - находим нашу кнопку
-	$(".next-slide").click(function(){
-		owl.trigger("next.owl.carousel");
-	});
-	$(".prev-slide").click(function(){
-		owl.trigger("prev.owl.carousel");
-	});
-
-$(function(){
-    $('a[href*=#]').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-        && location.hostname == this.hostname) {
-            var $target = $(this.hash);
-            $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
-            if ($target.length) {
-                var targetOffset = $target.offset().top;
-                $('html,body').animate({scrollTop: targetOffset}, 500);//скорость прокрутки
-                return false;
-            }
-        }
-    });
-});
+    
+    var owl=$(".owl-carousel");
+        owl.owlCarousel();
+        //$(".next") - находим нашу кнопку
+        $(".next-slide").click(function(){
+            owl.trigger("next.owl.carousel");
+        });
+        $(".prev-slide").click(function(){
+            owl.trigger("prev.owl.carousel");
+        });
+    
+ 
