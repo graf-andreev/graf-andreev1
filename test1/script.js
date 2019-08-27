7391810e7841ace7e2ffcc616f1e6f9c25dddff7
Maksim Andreev <46867573+graf-andreev@users.noreply.github.com>
2019-08-27 10:38:50 +0700

    Update script.js

--- test1/script.js
+++ test1/script.js
@@ -5,15 +5,10 @@
     function myFunction() {
        var val = document.getElementById("frame-slider").value
         document.getElementById('output').innerHTML = Math.abs(val-15)
+        document.getElementByClassName('value').innerHTML = val
     }
 
-    var follower = document.getElementById('slider-follow');
-    var follower_val = document.getElementById('slider-val');
-    var slider = document.getElementById('frame-slider');
+
     
-    var updateFollowerValue = function(val) {
-      follower_val.innerHTML = val;
-      follower.style.left = val + '%';
-    };
-    
-    updateFollowerValue(slider.value);
+
+   
