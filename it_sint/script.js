d0df22e97b6171da17260ee9b3c7defc541bc10c
Maksim Andreev <46867573+graf-andreev@users.noreply.github.com>
2019-09-23 17:05:38 +0700

    .

--- it_sint/script.js
+++ it_sint/script.js
@@ -40,14 +40,26 @@ pagination: {
 },
 
 // Navigation arrows
-// navigation: {
-// nextEl: '.swiper-button-next',
-// prevEl: '.swiper-button-prev',
-// },
+breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 40
+    },
 
 // And if we need scrollbar
 scrollbar: {
 el: '.swiper-scrollbar',
 dynamicBullets: true,
 },
-})
+}})
