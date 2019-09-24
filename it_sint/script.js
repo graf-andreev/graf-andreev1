1aba5f1fe51e73014a97dba58d74d2949c0ca788
Maksim Andreev <46867573+graf-andreev@users.noreply.github.com>
2019-09-24 13:39:05 +0700

    Update script.js

--- it_sint/script.js
+++ it_sint/script.js
@@ -44,17 +44,17 @@ breakpoints: {
     // when window width is >= 320px
     320: {
       slidesPerView: 1,
-      spaceBetween: 20
+      spaceBetween: 2
     },
     // when window width is >= 480px
     480: {
       slidesPerView: 3,
-      spaceBetween: 20
+      spaceBetween: 2
     },
     // when window width is >= 640px
     640: {
       slidesPerView: 3,
-      spaceBetween: 20
+      spaceBetween: 2
     },
 
 // And if we need scrollbar
