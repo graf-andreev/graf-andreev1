11b813bb142604636f9355ea8c4715effe65f358
Maxim <stat1c@ngs.ru>
2019-10-27 23:34:11 +0700

    .

--- colibri/js/script.js
+++ colibri/js/script.js
@@ -338,3 +338,23 @@ $(window).scroll(function() {
   }
 });
 });
+
+
+var acc = document.querySelectorAll("#why-mob");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = document.getElementsByClassName("mobile-content")
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
