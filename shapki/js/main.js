3e856ea57d6315c4ae38e4ded0bb7da5797d29e2
Maksim Andreev <46867573+graf-andreev@users.noreply.github.com>
2019-11-27 15:27:18 +0700

    .

--- shapki/js/main.js
+++ shapki/js/main.js
@@ -1,10 +1,16 @@
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
+    $(".slide-one").owlCarousel({
         loop:true,
         margin:10,
         nav:true,
         items: 1,
         dots: true
     });
-
+    $(".slide-two").owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        items: 4,
+        dots: true
+    });
   });
