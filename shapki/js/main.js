fb4de8dc726567df047d2ba02afb946f453ce04d
Maksim Andreev <46867573+graf-andreev@users.noreply.github.com>
2019-11-28 17:44:07 +0700

    .

--- shapki/js/main.js
+++ shapki/js/main.js
@@ -11,18 +11,26 @@ $(document).ready(function(){
         margin:10,
         nav:true,
         items: 4,
-        dots: true
+        dots: false
     });
   });
 
-  (function($) {
-    $(function() {
-     
-      $('div.tabs_caption').on('click', 'div:not(.active)', function() {
-        $(this)
-          .addClass('active').siblings().removeClass('active')
-          .closest('section.tabs').find('div.tabs_content').removeClass('active').eq($(this).index()).addClass('active');
-      });
-     
-    });
-    })(jQuery);
+
+$(function () {
+
+  $('div.tabs_caption').on('click', 'div:not(.active)', function () {
+    $(this)
+      .addClass('active').siblings().removeClass('active')
+      .closest('section.tabs').find('div.tabs_content').removeClass('active').eq($(this).index()).addClass('active');
+  });
+});
+
+$(function () {
+
+  $('div.season-toggle').on('click', 'div:not(.active)', function () {
+    $(this)
+      .addClass('active').siblings().removeClass('active')
+      .closest('section.seasons').find('div.seasons-content').removeClass('active').eq($(this).index()).addClass('active');
+  });
+});
+
