5983a7e55d8d517140398cce4563a4f801094fde
Maxim <stat1c@ngs.ru>
2019-11-30 22:40:50 +0700

    .

--- chisto5/js/main.js
+++ chisto5/js/main.js
@@ -2,8 +2,18 @@ $('.popup-on').click(function() {
   var popup_id = $('.popup'); 
   $(popup_id).show(); 
   $('.overlay').show(); 
-});
 
 $('.close').click(function () {
   $('.overlay, .popup').hide();
+});
+});
+
+$('#menu-open').click(function() { 
+  var menu = $('.aside-menu'); 
+  $(menu).css('left', '60%'); 
+
+$('#menu-close').click(function () {
+  var menu = $('.aside-menu'); 
+  $(menu).css('left', '100%');
+});
 });
