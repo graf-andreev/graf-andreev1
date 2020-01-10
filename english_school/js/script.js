5cd1fd73c8232cbf7bda534418d464ddbc1ab8e3
Maxim <stat1c@ngs.ru>
2020-01-10 15:02:07 +0700

    portfolio fixed

--- english_school/js/script.js
+++ english_school/js/script.js
@@ -138,115 +138,22 @@ $(function() {
         $(popup_id).fadeIn(400);
         $('.overlay_popup').show(); 
     });
-		$('.test-result').click(function() {
-        var popup_id = $('.popup-result');
-				$('.overlay_popup, .popup-test').hide();
-        $(popup_id).show();
-        $('.overlay_popup').show();
-    });
-		$('.img-close').click(function() {
-			$('.overlay_popup, .popup-result').hide();
-		});
     $('.img-close').click(function() { 
         $('.overlay_popup, .popup-info').hide();
     });
 		$('.img-close').click(function() {
 			$('.overlay_popup, .popup-complete').hide();
 		});
-    $('.choise-button').click(function() {
-    	  let course = $('.popup-course');
-	      let titel = $(this).parent().find('.name-choice').text();
-	      course.find('h4').text(titel);
-	      let text = '';
-	      if(titel === 'Английский для путешествий'){
-		      text = "Английский для путешествий Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium rspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totamrspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totamdoloremque laudantium, totam";
-	      }
-		    if(titel === 'Английский для начинающих'){
-			    text = "Английский для начинающих Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium rspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totamrspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totamdoloremque laudantium, totam";
-		    }
-		    if(titel === 'Бизнес английский'){
-			    text = "Бизнес английский Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium rspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totamrspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totamdoloremque laudantium, totam";
-		    }
-		    if(titel === 'Разговорный английский'){
-			    text = "Разговорный английский Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium rspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totamrspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totamdoloremque laudantium, totam";
-		    }
-	      course.find('.popup-text').text(text);
-		    $('#direction').val(titel);
-        $(course).fadeIn(400);
-        $('.overlay_popup').show(); 
-    });
-    $('.img-close').click(function() { 
-        $('.overlay_popup, .popup-course').hide();
-    });
 
     function burger() {
         var menu = document.querySelector('nav');
         menu.classList.toggle("show");
      }
 
-    $('.test > button').click(function() {
-    	let result = $('#result-points');
-	    var popup_id = '';
-	    if(result.text() !== ''){
-	    	popup_id = $('.popup-result');
-	    }else {
-		    popup_id = $('.popup-test');
-	    }
-        $(popup_id).fadeIn(500);
-        $('.overlay_popup').show();
-    });
-
     $('.img-close').click(function() { 
         $('.overlay_popup, .popup-test').hide();
-    });
-
-    $('.level1').click(function() {
-        var popup_id = $('.popup-level1');
-        $(popup_id).show();
-        $('.overlay_popup').show();
-    });
-    $('.img-close').click(function() {
-        $('.overlay_popup, .popup-level1').hide();
-    });
-
-		$('.level2').click(function() {
-			var popup_id = $('.popup-level2');
-			$(popup_id).show();
-			$('.overlay_popup').show();
-		});
-		$('.img-close').click(function() {
-			$('.overlay_popup, .popup-level2').hide();
-		});
-
-		$('.level3').click(function() {
-			var popup_id = $('.popup-level3');
-			$(popup_id).show();
-			$('.overlay_popup').show();
-		});
-		$('.img-close').click(function() {
-			$('.overlay_popup, .popup-level3').hide();
-		});
-
-		$('.level4').click(function() {
-			var popup_id = $('.popup-level4');
-			$(popup_id).show();
-			$('.overlay_popup').show();
-		});
-		$('.img-close').click(function() {
-			$('.overlay_popup, .popup-level4').hide();
-		});
-
-		$('.level5').click(function() {
-			var popup_id = $('.popup-level5');
-			$(popup_id).show();
-			$('.overlay_popup').show();
-		});
-		$('.img-close').click(function() {
-			$('.overlay_popup, .popup-level5').hide();
-		});
-
-
-
+	});
+	
 function timer(f) {
 	var date = new Date(f.replace(/-/g, '/'));
 	var f_time = Date.parse(date);
@@ -306,110 +213,3 @@ $(document).ready(function() {
 	let time = $(".timer-container").attr("data-finish");
 	timer(time);
 });
-
-
-$( document ).ready(function() {
-	$("form #btn").click(function(){
-		let form = $(this).parents('form');
-		let id = form.attr('id');
-		let button = $(this);
-
-		let nameInput = $("#"+id).find($('input[name="name"]'));
-		let phoneInput = $("#"+id).find($('input[name="phone"]'));
-		let adresInput = $("#"+id).find($('select[name="adres"]'));
-
-		let name = nameInput.val();
-		let phone = phoneInput.val();
-		let adres = adresInput.val();
-
-		if(id !== 'ajax_form2' && name!== '' && phone!== '' && adres!== null){
-			sendAjaxForm(id,button);
-			nameInput.css({'border': '0px solid'});
-			phoneInput.css({'border': '0px solid'});
-			adresInput.css({'border': '0px solid'});
-		}
-		if((id !== 'ajax_form2') && (name=== '' || phone=== '' || adres=== null)){
-			if(name=== ''){
-				nameInput.css({'border': '1px solid','border-color': 'red'});
-			}else{
-				nameInput.css({'border': '0px solid'});
-			}
-			if(phone=== ''){
-				phoneInput.css({'border': '1px solid','border-color': 'red'});
-			}else{
-				phoneInput.css({'border': '0px solid'});
-			}
-			if(adres=== null){
-				adresInput.css({'border': '1px solid','border-color': 'red'});
-			}else{
-				adresInput.css({'border': '0px solid'});
-			}
-		}
-		if(id === 'ajax_form2' && name!== '' && phone!== ''){
-			sendAjaxForm(id,button);
-			nameInput.css({'border': '0px solid'});
-			phoneInput.css({'border': '0px solid'});
-		}
-		if((id === 'ajax_form2') && (name=== '' || phone=== '')) {
-			if(name=== ''){
-				nameInput.css({'border': '1px solid','border-color': 'red'});
-			}else{
-				nameInput.css({'border': '0px solid'});
-			}
-			if(phone=== ''){
-				phoneInput.css({'border': '1px solid','border-color': 'red'});
-			}else{
-				phoneInput.css({'border': '0px solid'});
-			}
-		}
-
-		return false;
-	});
-});
-
-function sendAjaxForm(ajax_form,button) {
-	$.ajax({
-		url:     "/sender.php",
-		type:     "POST",
-		dataType: "html",
-		data: $("#"+ajax_form).serialize(),
-		success: function(response) {
-			$('.overlay_popup, .popup-group').hide();
-			$('.overlay_popup, .popup-info').hide();
-			$('.overlay_popup, .popup-test').hide();
-			$('.overlay_popup, .popup-result').hide();
-			$('.overlay_popup, .popup-adress').hide();
-			$('.overlay_popup, .popup-course').hide();
-			var popup_id = $('.popup-complete');
-			$(popup_id).show();
-			$('.overlay_popup').show();
-			$('input[name="name"]').val("");
-			$('input[name="phone"]').val("");
-			$('input[name="time"]').val("");
-			$('select[name="adres"]').val(null);
-		},
-	});
-}
-
-$( document ).ready(function() {
-
-	$("#test-start").click(function () {
-		$("#test-on").val('on');
-	});
-	$(".teachers-text").click(function () {
-		let teacherName = $(this).parent().find('.teacher-name').html();
-		let name = $(this).parent().find('.teacher-name').data('name');
-		let info = $('#' + name).html();
-		var popup_id = $('.popup-teachers');
-		popup_id.find('.text').html(info);
-		popup_id.find('h4').html(teacherName);
-		$(popup_id).fadeIn(400);
-		$('.overlay_popup').show();
-	});
-	$('.img-close').click(function () {
-		$('.overlay_popup, .popup-teachers').hide();
-	});
-});
-
-
-
