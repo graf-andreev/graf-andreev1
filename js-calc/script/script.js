16500120f5587214d40c0d27c1e5cde1fd988fb1
Maxim <stat1c@ngs.ru>
2020-02-19 16:37:26 +0700

    .

--- js-calc/script/script.js
+++ js-calc/script/script.js
@@ -1,4 +1,41 @@
-const startButton = document.querySelector('.start-button');
+const startButton = document.querySelector('.start-button'),
+    firstScreen = document.querySelector('.first-screen'),
+    mainForm = document.querySelector('.main-form'),
+    formCalculate = document.querySelector('.form-calculate'),
+    endButton = document.querySelector('.end-button'),
+    total = document.querySelector('.total'),
+    fastRange = document.querySelector('.fast-range');
 
+function showElem(elem) {
+    elem.style.display = 'block';
+}
 
-console.dir(startButton)
+function hideElem(elem) {
+    elem.style.display = 'none';
+}
+
+startButton.addEventListener('click', function() {
+    showElem(mainForm);
+    hideElem(firstScreen);
+});
+
+endButton.addEventListener('click', function() {
+    
+    for(const elem of formCalculate.elements){
+        if (elem.tagName === 'FIELDSET'){
+            hideElem(elem);
+        }
+    }
+    showElem(total);
+
+});
+
+function handlerCallBackForm(event) {
+    const target = event.target;
+    if (target.classList.contains('want-faster')) {
+        target.checked ? showElem(fastRange) : hideElem(fastRange);
+    }
+    
+};
+
+formCalculate.addEventListener('change', handlerCallBackForm);
