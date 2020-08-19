9e957f76ac24a6a3010df13ac8ca007994383dc5
Maksim <stat1c@ngs.ru>
2020-08-20 00:09:37 +0700

    PWA

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-"use strict";
+'use strict';
 
-importScripts('/sw-toolbox.js');
+importScripts('sw-toolbox.js');
 
-toolbox.precache(["/index.html","/css/style.css"]);
+toolbox.precache(["index.html","style/style.css"]);
 
-toolbox.router.get('/img/*', toolbox.cacheFirst);
+toolbox.router.get('/images/*', toolbox.cacheFirst);
 
 toolbox.router.get('/*', toolbox.networkFirst, {
   networkTimeoutSeconds: 5
