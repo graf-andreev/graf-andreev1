20400aeeabbee7cd9eca791fa4fb648648d0ef3d
Maxim <stat1c@ngs.ru>
2020-12-18 22:24:43 +0700

    farm

---
+++ farm-razvitie/js/script.js
@@ -0,0 +1,38 @@
+jQuery(document).ready(function ($) {
+    var owl = $('.gallery-top');
+    owl.owlCarousel({
+        nav: false,
+        dots: false,
+        mouseDrag: false,
+        items: 1,
+        loop: true,
+        margin: 0,
+        responsive: {
+            300: {
+                items: 1,
+                margin: 0,
+                mouseDrag: true,
+                nav: false,
+                dots: true,
+            },
+            850: {
+                items: 1,
+                mouseDrag: true,
+                dots: false,
+                nav: false,
+            }
+        }
+    });
+
+    owl.on('drag.owl.carousel', function (event) {
+        document.ontouchmove = function (e) {
+            e.preventDefault();
+        }
+    });
+
+    owl.on('dragged.owl.carousel', function (event) {
+        document.ontouchmove = function (e) {
+            return true;
+        }
+    });
+});
